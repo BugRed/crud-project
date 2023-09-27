6558af5a729bf358d12568a1e36bce3cd9365339
unknown <o.adrian.rodrigues@gmail.com>
2023-09-26 22:28:45 -0300

    add tables

--- script.js
+++ script.js
@@ -1,40 +1,122 @@
 const placeName = document.querySelectorAll('input[name=author]')
 const placePrice = document.querySelectorAll('input[name=price]')
+const placeQuantity = document.querySelectorAll('input[name=pass]')
 
-const test = document.getElementById('res')
-
+const td = document.querySelectorAll('td[name=td]')
 const btns = document.querySelectorAll('input[type=button]')
 
-const listProduct = [{}];
+const listProduct = [{}]
+// const listEmployee = [{}]
 
-const listEmployee = [{}]
+// console.log(td[0].textContent)
 
 
 const showResult = () => {
-    console.log(list)
+    console.log(listProduct)
 }
 
 btns.forEach((element) => {
     element.onclick = () => {
+        // && listEmployee != null
+        if (listProduct != null) {
+            if (element.classList[1] === 'product') {
+                addItemToList(listProduct, placeName[0], placePrice[0], placeQuantity[0])
+                // addRow('table-view');
+                addTable()
+                // console.log('done')
 
-        if (element.classList[1] === 'product') {
-            sum(listProduct, placeName[0], placePrice[0])
-        } if (element.classList[1] === 'employee') {
-            sum(listEmployee, placeName[1], placePrice[1])
+                // } if (element.classList[1] === 'employee') {
+                //     addItemToList(listEmployee, placeName[1], placePrice[1], placeQuantity[0])
+                // }
+            }
+            // showResult()
         }
-        console.log(listProduct)
-        console.log(listEmployee)
+
     }
-
-
-
 })
 
-function sum(buttons, names, prices) {
-    buttons[0].name = names.value
-    buttons[0].price = prices.value
-    test.innerText = `A lista de Produtos é ${buttons[0].name} no preço: ${buttons[0].price}`
+const addItemToList = (buttons, names, prices, quantity) => {
+    buttons.push({
+        name: names.value,
+        price: prices.value,
+        quantity: quantity.value
+    })
 }
+
+//Criar uma nova linha numa tabela
+function addRow(tableID) {
+
+    let tableRef = document.getElementById(tableID);
+
+    let newRow = tableRef.insertRow();
+    let newRow1 = tableRef.insertRow();
+    let newRow2 = tableRef.insertRow();
+
+
+    let newCell = newRow.insertCell();
+    let newCell1 = newRow1.insertCell();
+    let newCell2 = newRow2.insertCell();
+
+    alinhando(newCell)
+    alinhando(newCell1)
+    alinhando(newCell2)
+
+    let newText = document.createTextNode("New bottom row");
+    let newText1 = document.createTextNode("New bottom row1");
+    let newText2 = document.createTextNode("New bottom row2");
+    newCell.appendChild(newText);
+    newCell1.appendChild(newText1);
+    newCell2.appendChild(newText2);
+}
+
+
+function changeColor(td) {
+    td.style.backgroundColor = "#FF0F0F";
+}
+
+function alinhando(td) {
+    td.style.padding = '5px 100px 5px 5px'
+    td.style.border = 'solid grey 1px'
+    // changeColor(td)
+}
+
+
+function addTable() {
+  // cria um novo elemento div
+  
+  var newTable = document.createElement("table");
+
+  // e dá à ele conteúdo
+  var newRow = newTable.insertRow();
+  var newRow1 = newTable.insertRow();
+  var newRow2 = newTable.insertRow();
+
+
+  let newCell = newRow.insertCell();
+  let newCell1 = newRow.insertCell();
+  let newCell2 = newRow.insertCell();
+  
+
+  alinhando(newCell)
+  var newText = document.createTextNode(listProduct[1].name);
+  var newText2 = document.createTextNode(listProduct[1].price);
+  var newText3 = document.createTextNode(listProduct[1].quantity);
+
+  
+  newCell.appendChild(newText);
+  newCell1.appendChild(newText2);
+  newCell2.appendChild(newText3);
+
+  // adiciona o novo elemento criado e seu conteúdo ao DOM
+  var divAtual = document.getElementById("div1");
+  document.body.insertBefore(newTable, divAtual);
+
+  
+  
+  console.log(newTable)
+}
+
+
 
 
 
